8be8515c322053f44b2913196bd0b9511ad27ce7
Behic Akgun <behica@gmail.com>
2017-04-12 18:32:39 +0000

    Checkpoint 6 complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -9,15 +9,16 @@ class AddressBook
 
 
 
-def add_entry(name, phone_number, email)
-    index = 0
-    entries.each do |entry|
-        if name < entry.name
-            break
+    def add_entry(name, phone_number, email)
+        index = 0
+        entries.each do |entry|
+            if name < entry.name
+                break
+            end
+            
+            index += 1
         end
-        index += 1
-    end
 
-    entries.insert(index, Entry.new(name, phone_number, email))
-end
+        entries.insert(index, Entry.new(name, phone_number, email))
+    end
 end
